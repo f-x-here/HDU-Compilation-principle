00bafe8ae89fe92bf42196b9b99d4cf640e5c653
fxhere <3166927037@qq.com>
2024-11-25 21:28:54 +0800

    2

--- 实验3/MethodForExtractingLeftCommonFactorsInGrammar.cpp
+++ 实验3/MethodForExtractingLeftCommonFactorsInGrammar.cpp
@@ -2,19 +2,24 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <queue>
 using namespace std;
-struct piece {
-    char data;
-    vector<piece*>next;
+struct piece
+{
+    string data;
+    vector<piece *> next;
 };
-void deleteTree(piece* node) {
-    if (node == nullptr) return;
-    for (auto& child : node->next) {
+void deleteTree(piece *node)
+{
+    if (node == nullptr)
+        return;
+    for (auto &child : node->next)
+    {
         deleteTree(child);
     }
     delete node;
 }
-void showTree(piece* node, string format)
+void showTree(piece *node, string format)
 {
     for (int i = 0; i < format.length(); i++)
     {
@@ -41,15 +46,64 @@ void showTree(piece* node, string format)
     }
 }
 
+void ExtractingLeftCommonFactors(piece *head)
+{
+    piece *cur, *next;
+    cur = head;
+    queue<piece *> q;
+    q.push(cur);
+    int i = 0, k = 0;
+    while (!q.empty())
+    {
+        cur = q.front();
+        q.pop();
+        cout << "(" <<head->data;
+        for (int j = 0; j < i; j++)
+        {
+            cout << "'";
+        }
+        cout << ") -> ";
+        for (int l = 0 ;l<cur->next.size();l++)
+        {
+            next = cur->next[l];
+            while (next->next.size() == 1)
+            {
+                cout << next->data;
+                next = next->next[0];
+            }
+            if (next->next.size() > 1)
+            {
+                cout << next->data;
+                q.push(next);
+                k++;
+                cout << "(" << head->data;
+                for (int j = 0; j < k; j++)
+                {
+                    cout << "'";
+                }
+                cout<<")";
+            }
+            else
+            {
+                cout << next->data;
+            }
+            if(l!=cur->next.size()-1)
+            cout<<" | ";
+        }
+        cout<<endl;
+        i++;
+    }
+}
 
 int main()
 {
     string expr;
+    vector<piece *> heads;
     while (getline(cin, expr))
     {
-        piece* head = new piece;
+        piece *head = new piece;
         head->data = expr[0];
-        piece* cur = head;
+        piece *cur = head;
         bool check;
         for (int i = 1; i < expr.length(); i++)
         {
@@ -62,9 +116,9 @@ int main()
                 check = false;
                 if (!cur->next.empty())
                 {
-                    for (auto& it : cur->next)
+                    for (auto &it : cur->next)
                     {
-                        if (it != NULL && it->data == expr[i])
+                        if (it != NULL && it->data[0] == expr[i])
                         {
                             cur = it;
                             check = true;
@@ -74,15 +128,21 @@ int main()
                 }
                 if (!check)
                 {
-                    piece* newPiece = new piece;
+                    piece *newPiece = new piece;
                     newPiece->data = expr[i];
                     cur->next.push_back(newPiece);
                     cur = newPiece;
                 }
             }
         }
-        showTree(head, "");
-        deleteTree(head);
+        heads.push_back(head);
     }
-
+    for(auto &h:heads)
+    {
+        cout<<endl;
+        showTree(h, "");
+        ExtractingLeftCommonFactors(h);
+        cout<<endl;
+    }
 }
+// S -> apple | apply | application | battle | bat | brother | bX
